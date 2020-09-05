3287baa6407b2a22f911ee39caf54ae2ac8570c1
JSEC <juansec13@gmail.com>
2020-09-05 11:20:42 -0500

    BACK Y FRONT

---
+++ ejemplo01Jav/src/main/java/com/sinfloo/ejemplo01/PersonaService.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sinfloo.ejemplo01;
+import java.util.List;
+/**
+ *
+ * @author USUARIO
+ */
+public interface PersonaService {
+    List<Persona>listar();
+    
+    Persona listarId(int id);
+    Persona add(Persona p);
+    Persona edit(Persona p);
+    Persona delete(int id);
+    
+    
+}
---
+++ ejemplo01Jav/src/main/java/com/sinfloo/ejemplo01/PersonaServiceImp.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sinfloo.ejemplo01;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author USUARIO
+ */
+@Service
+public class PersonaServiceImp implements PersonaService{
+
+    @Autowired
+    private PersonaRepositorio repositorio;
+    
+    
+    
+    @Override
+    public List<Persona> listar() {
+        return repositorio.findAll(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Persona listarId(int id) {
+        return repositorio.findById(id); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Persona add(Persona p) {
+        return repositorio.save(p); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Persona edit(Persona p) {
+        return repositorio.save(p);//To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Persona delete(int id) {
+        Persona p = repositorio.findById(id);
+        if(p!=null)
+        {
+            repositorio.delete(p);
+        }
+        
+        return p; //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
